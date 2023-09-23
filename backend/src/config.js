81b1c20001f8a83a930ab537c5ce280aa2014606
Cristian Alexander Beltran Paco <cristian.beltran.paco@gmail.com>
2023-09-22 22:07:47 -0400

    config local

--- backend/src/config.js
+++ backend/src/config.js
@@ -1,1 +1,9 @@
 export const TOKEN_SECRET = "tokenSecret";
+export const DB = "envibol";
+export const USERNAME = "postgres";
+export const PASSWORD = "holamundo";
+export const HOST = "localhost"
+//export const HOST = "srv-captain--prodem-db"
+export const PORT = 5432;
+
+ 
--- backend/src/db.js
+++ backend/src/db.js
@@ -1,7 +1,9 @@
 import Sequelize from "sequelize";
+import { DB, HOST, PASSWORD, PORT, USERNAME } from "./config.js";
 
-export const sequelize = new Sequelize("envibol", "postgres", "holamundo1", {
-  host: "srv-captain--envibol-db",
+export const sequelize = new Sequelize(DB, USERNAME, PASSWORD, {
+  host: HOST,
+  port: PORT,
   dialect: "postgres",
   timezone: "-04:00",
 });
