77a6d0ae9901ac2348bd761da86807b81a014c78
mattahuasi <mattahuasi@gmail.com>
2023-10-01 00:31:48 -0400

    Agregado Turnstile.vue con nueva versión de Vue más import de Gi Icons en el main.js

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,6 +1,7 @@
 import "@/assets/index.css";
 import * as FaIcons from "oh-vue-icons/icons/fa";
 import * as CoIcons from "oh-vue-icons/icons/co";
+import * as GiIcons from "oh-vue-icons/icons/gi";
 import { createApp } from "vue";
 import { createPinia } from "pinia";
 
@@ -10,8 +11,9 @@ import { OhVueIcon, addIcons } from "oh-vue-icons";
 
 const Co = Object.values({ ...CoIcons });
 const Fa = Object.values({ ...FaIcons });
+const Gi = Object.values({ ...GiIcons });
 
-addIcons(...Fa, ...Co);
+addIcons(...Fa, ...Co, ...Gi);
 
 const app = createApp(App);
 
