bf0320cc545ec103c1cb2aa5206e9e06297ed038
maccXX98 <dahakar1998@gmail.com>
2023-10-03 14:30:13 -0400

    correcciones

--- backend/src/schemas/employee.schemas.js
+++ backend/src/schemas/employee.schemas.js
@@ -32,15 +32,16 @@ export const employeeSchema = z.object({
       required_error: "El CI es requerido",
     })
     .min(4, {
-      message: "El CI debe tener al menos 6 caracteres",
+      message: "El CI debe tener al menos 4 caracteres",
     })
     .max(15, {
       message: "El CI debe tener como máximo 15 caracteres",
     }),
-  address: z.string({
-    required_error: "La dirección es requerida",
-    invalid_type_error: "La dirección debe ser un string",
-  }),
+  address: z
+    .string({
+      required_error: "La dirección es requerida",
+      invalid_type_error: "La dirección debe ser un string",
+    }),
   telf: z.string(),
   cel: z.string(),
   email: z
@@ -50,23 +51,20 @@ export const employeeSchema = z.object({
     .email({
       message: "Email invalido",
     }),
-  password: z.string({
-    required_error: "Ingrese una contraseña",
-    min_length_error: "La contraseña debe tener al menos 6 caracteres",
-    max_length_error: "La contraseña no debe tener más de 20 caracteres",
-    strength_error:
-      "La contraseña debe incluir al menos una letra mayúscula, una letra minúscula y un número",
-    min_length: 6,
-    max_length: 20,
-    strength: {
-      uppercase: true,
-      lowercase: true,
-      numbers: true,
-    },
-  }),
-  roleId: z.string({
-    required_error: "El rol es requerido",
-  }),
+  password: z
+    .string({
+      min_length: 6,
+      max_length: 20,
+      pattern: /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9]).{6,20}$/,
+      required_error: "Por favor, ingrese una contraseña.",
+      min_length_error: "La contraseña debe tener al menos 6 caracteres.",
+      max_length_error: "La contraseña no debe tener más de 20 caracteres.",
+      pattern_error: "La contraseña debe incluir al menos una letra mayúscula, una letra minúscula y un número (por ejemplo, 'Abc123').",
+    }),
+  roleId: z
+    .string({
+      required_error: "El rol es requerido",
+    }),
   staff: z.boolean(),
   admin: z.boolean(),
 });
