2857f29b2f34609ce65c2454b1b855bca0dc9922
mattahuasi <mattahuasi@gmail.com>
2023-10-05 23:28:06 -0400

    Actualizado ExternalForms.vue

--- frontend/src/views/forms/ExternalForms.vue
+++ frontend/src/views/forms/ExternalForms.vue
@@ -1,253 +1,139 @@
 <template>
-  <div class="flex flex-wrap mt-4">
-    <div class="w-full mb-12 px-4">
-      <div
-        class="relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded-lg bg-blueGray-100 border-0"
-      >
-        <div class="rounded-t bg-white mb-0 px-6 py-6">
-          <div class="text-center flex justify-between">
-            <h6 class="text-blueGray-700 text-xl font-bold">Usuario externo</h6>
-          </div>
-        </div>
-        <div class="flex-auto px-4 lg:px-10 py-10 pt-0">
-          <form :onSubmit="handleSubmit">
-            <h6 class="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
-              Datos de visitante
-            </h6>
-            <div v-if="alertOpen">
-              <div
-                v-for="(item, index) in errors"
-                :key="index"
-                className="bg-red-500 p-2 text-white rounded-lg mb-2 text-center"
-              >
-                {{ item }}
-              </div>
-            </div>
-            <div class="flex flex-wrap">
-              <div class="w-full lg:w-6/12 px-4">
-                <div class="relative w-full mb-3">
-                  <label
-                    class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                    htmlFor="grid-password"
-                  >
-                    Nombre/s
-                  </label>
-                  <div
-                    class="p-1 mb-1"
-                    v-for="(error, index) of v$.formData.first_name.$errors"
-                    :key="index"
-                  >
-                    <p class="text-sm text-red-500">{{ error.$message }}</p>
-                  </div>
-                  <input
-                    v-model="v$.formData.first_name.$model"
-                    type="text"
-                    class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                  />
-                </div>
-              </div>
-
-              <div class="w-full lg:w-6/12 px-4">
-                <div class="relative w-full mb-3">
-                  <label
-                    class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                  >
-                    Apellidos
-                  </label>
-                  <div
-                    class="p-1 mb-1"
-                    v-for="(error, index) of v$.formData.last_name.$errors"
-                    :key="index"
-                  >
-                    <p class="text-sm text-red-500">{{ error.$message }}</p>
-                  </div>
-                  <input
-                    v-model="v$.formData.last_name.$model"
-                    type="text"
-                    class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                  />
-                </div>
-              </div>
-              <div class="w-full lg:w-6/12 px-4">
-                <div class="relative w-full mb-3">
-                  <label
-                    class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                  >
-                    CI
-                  </label>
-                  <div
-                    class="p-1 mb-1"
-                    v-for="(error, index) of v$.formData.ci.$errors"
-                    :key="index"
-                  >
-                    <p class="text-sm text-red-500">{{ error.$message }}</p>
-                  </div>
-                  <input
-                    v-model="v$.formData.ci.$model"
-                    type="text"
-                    class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                  />
-                </div>
-              </div>
-            </div>
-            <hr class="mt-6 border-b-1 border-blueGray-300" />
-
-            <h6 class="text-blueGray-400 text-sm mt-3 mb-6 font-bold uppercase">
-              Informacion de contacto
-            </h6>
-            <div class="flex flex-wrap">
-              <div class="w-full lg:w-12/12 px-4">
-                <div class="relative w-full mb-3">
-                  <label
-                    class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                  >
-                    Direccion
-                  </label>
-                  <input
-                    type="text"
-                    v-model="v$.formData.address.$model"
-                    class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                  />
-                </div>
-              </div>
-              <div class="w-full lg:w-6/12 px-4">
-                <div class="relative w-full mb-3">
-                  <label
-                    class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                  >
-                    Telefono
-                  </label>
-                  <input
-                    type="number"
-                    v-model="v$.formData.telf.$model"
-                    class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                  />
-                </div>
-              </div>
-
-              <div class="w-full lg:w-6/12 px-4">
-                <div class="relative w-full mb-3">
-                  <label
-                    class="block uppercase text-blueGray-600 text-xs font-bold mb-2"
-                  >
-                    Celular
-                  </label>
-                  <input
-                    v-model="v$.formData.cel.$model"
-                    type="number"
-                    class="border-0 px-3 py-3 placeholder-blueGray-300 text-blueGray-600 bg-white rounded text-sm shadow focus:outline-none focus:ring w-full ease-linear transition-all duration-150"
-                  />
-                </div>
-              </div>
-            </div>
-
-            <div class="w-full mx-auto p-4 md:py-8">
-              <div class="flex items-center justify-between">
-                <div class="text-center flex items-center mb-4">
-                  <router-link to="/admin/external" v-slot="{ href, navigate }">
-                    <button
-                      :href="href"
-                      :click="navigate"
-                      class="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                      type="button"
-                    >
-                      <span><i class="fas fa-arrow-left"></i> Atras</span>
-                    </button>
-                  </router-link>
-                </div>
-                <div class="text-center flex items-center mb-4">
-                  <button
-                    class="bg-blueGray-800 text-white active:bg-blueGray-600 text-sm font-bold uppercase px-6 py-3 rounded shadow hover:shadow-lg outline-none focus:outline-none mr-1 mb-1 ease-linear transition-all duration-150"
-                    type="submit"
-                  >
-                    <span>Guardar <i class="fas fa-save"></i></span>
-                  </button>
-                </div>
-              </div>
-            </div>
-          </form>
-        </div>
+  <Forms title="Usuario externo" icon="fa-user" @handleSubmit="handleSubmit">
+    <h6 class="text-gray-400 dark:text-gray-100 text-sm mt-3 mb-6 font-bold uppercase">
+      Datos de visitante
+    </h6>
+    <div class="flex flex-wrap">
+      <div class="w-full lg:w-6/12 px-4">
+        <Input id="first_name" 
+          labelText="Nombre/s" 
+          v-model="v$.first_name.$model" 
+          :errors="v$.first_name.$errors"
+          type="text" 
+        />
+      </div>
+      <div class="w-full lg:w-6/12 px-4">
+        <Input 
+          id="last_name" 
+          labelText="Apellidos" 
+          v-model="v$.last_name.$model" 
+          :errors="v$.last_name.$errors"
+          type="text" 
+        />
+      </div>
+      <div class="w-full lg:w-6/12 px-4">
+        <Input 
+          id="ci" 
+          labelText="CI" 
+          v-model="v$.ci.$model" 
+          :errors="v$.ci.$errors" 
+          type="text" 
+        />
       </div>
     </div>
-  </div>
+    <h6 class="text-gray-400 dark:text-gray-100 text-sm mt-3 mb-6 font-bold uppercase">
+      Información de contacto
+    </h6>
+    <div class="flex flex-wrap">
+      <div class="w-full lg:w-full px-4">
+        <Input 
+          id="address" 
+          labelText="Dirección" 
+          v-model="v$.address.$model" 
+          :errors="v$.address.$errors" 
+          type="text" 
+        />
+      </div>
+      <div class="w-full lg:w-6/12 px-4">
+        <Input 
+          id="telf" 
+          labelText="Telefóno" 
+          v-model="v$.telf.$model" 
+          :errors="v$.telf.$errors" 
+          type="number" 
+        />
+      </div>
+      <div class="w-full lg:w-6/12 px-4">
+        <Input 
+          id="cel" 
+          labelText="Celular" 
+          v-model="v$.cel.$model" 
+          :errors="v$.cel.$errors" 
+          type="number" 
+        />
+      </div>
+    </div>
+  </Forms>
 </template>
-<script>
-import { useVuelidate } from "@vuelidate/core";
-import { required, helpers } from "@vuelidate/validators";
+<script setup>
 import {
   createExternalRequest,
   getExternalRequest,
   updateExternalRequest,
-} from "../../api/external";
+} from "@/api/external";
 
-export default {
-  setup() {
-    return { v$: useVuelidate() };
-  },
+import { useVuelidate } from "@vuelidate/core";
+import { useRoute, useRouter } from "vue-router";
+import { required, helpers, email } from "@vuelidate/validators";
+import { ref, onMounted, reactive } from "vue";
+import { toast } from "vue-sonner";
 
-  data() {
-    return {
-      formData: {
-        first_name: "",
-        last_name: "",
-        ci: "",
-        telf: "",
-        address: "",
-        cel: "",
-      },
-      errors: [],
-      alertOpen: false,
-    };
+import Forms from "@/components/Cards/Forms.vue";
+import Input from "@/components/Inputs/Input.vue";
+
+const route = useRoute();
+const router = useRouter();
+
+const formData = reactive({
+  first_name: "",
+  last_name: "",
+  ci: "",
+  address: "",
+  telf: "",
+  cel: "",
+});
+const rules = {
+  first_name: {
+    required: helpers.withMessage("Se requiere el nombre/s", required),
   },
-  validations() {
-    return {
-      formData: {
-        first_name: {
-          required: helpers.withMessage("Se requiere el nombre/s", required),
-        },
-        last_name: {
-          required: helpers.withMessage("Se requiere los apellidos", required),
-        },
-        ci: { required: helpers.withMessage("Se requiere el CI", required) },
-        address: {},
-        telf: {},
-        cel: {},
-      },
-    };
+  last_name: {
+    required: helpers.withMessage("Se requiere los apellidos", required),
   },
-  methods: {
-    notification() {
-      this.alertOpen = true;
-      const timer = setTimeout(() => {
-        this.alertOpen = false;
-      }, 3000);
-      return () => clearTimeout(timer);
-    },
-    handleSubmit(event) {
-      event.preventDefault();
-      this.v$.$touch();
-      if (!this.v$.$invalid) {
-        const request = async () => {
-          try {
-            if (!this.$route.query.id) {
-              await createExternalRequest(this.formData);
-            } else
-              await updateExternalRequest(this.$route.query.id, this.formData);
-            this.$router.push("/admin/external");
-          } catch (error) {
-            this.errors = error.response.data.errors;
-            this.notification();
-          }
-        };
-        request();
-      }
-    },
+  ci: {
+    required: helpers.withMessage("Se requiere el CI", required),
   },
-  async created() {
-    if (this.$route.query.id) {
-      const res = await getExternalRequest(this.$route.query.id);
-      this.formData = res.data;
+  address: {},
+  telf: {},
+  cel: {},
+}
+
+const v$ = useVuelidate(rules, formData);
+const errors = ref([]);
+
+async function handleSubmit() {
+  const isFormCorrect = await v$.value.$validate();
+  if (isFormCorrect) {
+    try {
+      if (!route.query.id)
+        await createExternalRequest(formData);
+      else
+        await updateExternalRequest(route.query.id, formData);
+      console.log(formData.value);
+      toast.success("Externo guardado correctamente");
+      router.push("/external");
+    } catch (error) {
+      errors.value = error.response.data.errors;
+      errors.value.map((item) => toast.error(item));
     }
-  },
-};
+  }
+}
+onMounted(async () => {
+  try {
+    const res = await getExternalRequest(route.query.id)
+    Object.assign(formData, res.data);
+  } catch (error) {
+    toast.error("Error al cargar datos");
+    route.push("/external");
+  }
+});
 </script>
